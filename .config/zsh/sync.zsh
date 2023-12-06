a04e050dee3d038feb091f76c243a0c16e00cea9
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-12-06 16:19:24 +0900

    Fix zsh history

--- .config/zsh/sync.zsh
+++ .config/zsh/sync.zsh
@@ -2,11 +2,14 @@
 # Zsh configuration
 # -----------------
 HISTFILE=${HOME}/.zsh_history
+HISTSIZE=1000
+SAVEHIST=10000
 setopt AUTO_CD
 setopt HIST_IGNORE_ALL_DUPS
 setopt SHARE_HISTORY
 setopt INC_APPEND_HISTORY
 setopt HIST_REDUCE_BLANKS
+setopt incappendhistory
 # Set editor default keymap to emacs (`-e`) or vi (`-v`)
 bindkey -v
 # Remove path separator from WORDCHARS.
