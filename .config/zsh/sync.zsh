637e645089710c85a06c40d100f6d13dd6ed5351
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-12-18 23:41:45 +0900

    Add PATH for snap

--- .config/zsh/sync.zsh
+++ .config/zsh/sync.zsh
@@ -15,3 +15,9 @@ setopt incappendhistory
 bindkey -v
 # Remove path separator from WORDCHARS.
 WORDCHARS=${WORDCHARS//[\/]}
+
+# Expand $PATH to include the directory where snappy applications go.
+snap_bin_path="/snap/bin"
+if [ -n "${PATH##*${snap_bin_path}}" -a -n "${PATH##*${snap_bin_path}:*}" ]; then
+    export PATH=$PATH:${snap_bin_path}
+fi
