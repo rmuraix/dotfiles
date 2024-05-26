ddbb82987095d02886516add7e97f852b81aedcf
Ryota Murai <35632215+rmuraix@users.noreply.github.com>
2024-05-26 20:28:59 +0900

    Add peco and ghq

--- .config/zsh/sync.zsh
+++ .config/zsh/sync.zsh
@@ -24,3 +24,13 @@ bindkey -v
 # Remove path separator from WORDCHARS.
 WORDCHARS=${WORDCHARS//[\/]}
 
+function peco-src () {
+  local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-src
+bindkey '^]' peco-src
